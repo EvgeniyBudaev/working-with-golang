c2f8c1e850ef8bad940d34ccc06db9a7e31aa1ac
Evgeniy Budaev <budaev.e@gmail.com>
2023-10-10 09:54:36 +0300

    feat: Looking up a user by email

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 )
@@ -39,6 +40,13 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 	err := app.readJSON(w, r, &requestPayload)
 	if err != nil {
 		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate user against database
+	user, err := app.DB.GetUserByEmail(requestPayload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
